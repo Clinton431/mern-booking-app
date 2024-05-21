e8ce90b96698f5e8028e0de4fa7e073f44080205
Clinton431 <clintonnyakoe@gmail.com>
2024-05-21 14:56:42 +0300

    added my hotels view and added e2e test

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -45,3 +45,18 @@ test("should allow user to add a hotel", async({ page })=> {
     await page.getByRole("button", { name: "Save" }).click();
     await expect(page.getByText("Hotel Saved!")).toBeVisible();
 });
+
+test("should display hotels", async({ page })=>{
+    await page.goto(`${UI_URL}my-hotels`);
+
+    await expect(page.getByText("Nairobi Getaway")).toBeVisible();
+    await expect(page.getByText("Lorem ipsum dolor sit amet")).toBeVisible();
+    await expect(page.getByText("Nairobi, Kenya")).toBeVisible();
+    await expect(page.getByText("All Inclusive")).toBeVisible();
+    await expect(page.getByText("£120 per night")).toBeVisible();
+    await expect(page.getByText("3 adults, 3 children")).toBeVisible();
+    await expect(page.getByText("4 Star Rating")).toBeVisible();
+
+    await expect(page.getByRole("link", { name: "View Details"})).toBeVisible();
+    await expect(page.getByRole("link", { name: "Add Hotel"})).toBeVisible();
+})
