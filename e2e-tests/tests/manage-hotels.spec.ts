020fd509ba408ad93fbafa791e8c941f0104932d
Clinton431 <clintonnyakoe@gmail.com>
2024-05-27 15:17:05 +0300

    added edit hotel feature and added e2e test

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -57,6 +57,25 @@ test("should display hotels", async({ page })=>{
     await expect(page.getByText("3 adults, 3 children")).toBeVisible();
     await expect(page.getByText("4 Star Rating")).toBeVisible();
 
-    await expect(page.getByRole("link", { name: "View Details"})).toBeVisible();
+    await expect(page.getByRole("link", { name: "View Details"}).first()).toBeVisible();
     await expect(page.getByRole("link", { name: "Add Hotel"})).toBeVisible();
-})
+});
+
+test("should edit hotel", async({ page })=>{
+    await page.goto(`${UI_URL}my-hotels`);
+
+    await page.getByRole("link", { name: "View Details" }).first().click();
+
+    await page.waitForSelector('[name="name"]', { state: "attached"})
+    await expect(page.locator('[name="name"]')).toHaveValue("Nairobi Getaways")
+    await page.locator('[name="name"]').fill("Nairobi Getaways UPDATED")
+    await page.getByRole("button", { name: "Save"}).click();
+    await expect(page.getByText("Hotel Saved!")).toBeVisible();
+
+    await page.reload();
+
+    await expect(page.locator('[name="name"]')).toHaveValue("Nairobi Getaways UPDATED");
+    await page.locator('[name="name"]').fill("Nairobi Gateaways");
+    await page.getByRole("button", { name: "Save" }).click();
+});
+
