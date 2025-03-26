1150b98963c078db45a2fe539c56bdf391af139a
Clinton431 <clintonnyakoe@gmail.com>
2025-03-26 16:28:20 +0300

    changes

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,16 +1,14 @@
 const Hero = () => {
-    return (
-        <div className="bg-blue-800 pb-16">
-            <div className="container mx-auto flex flex-col gap-2">
-                <h1 className="text-5xl text-white font-bold">
-                    Find your stay
-                </h1>
-                <p className="text-2xl text-white">
-                    Search low prices on hotels for your dream vacation...
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="bg-blue-500 pb-16">
+      <div className="container mx-auto flex flex-col gap-2">
+        <h1 className="text-5xl text-gray font-bold">Find your stay</h1>
+        <p className="text-2xl text-white">
+          Search for affordable priced hotels for your dream vacation...
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
